f44eb150e9ec786913c3e854bc95d79317f91d69
Erik Peterson <erik@thepetecollective.com>
2025-08-14 22:42:19 -0500

    tighten up ingredients and swap buns

--- src/BurgerStack.tsx
+++ src/BurgerStack.tsx
@@ -1,5 +1,6 @@
 import type { Ingredient } from "./IngredientsModel";
 import topBunPng from "./assets/bun-top.png"
+import topBunPng2 from "./assets/bun-top2.png"
 import bottomBunPng from "./assets/bun-bottom.png"
 
 interface Props {
@@ -8,9 +9,11 @@ interface Props {
 
 export function BurgerStack({ ingredients }: Props) {
 
+    const topBunSelect = ingredients.length === 0 ? topBunPng2 : topBunPng
+
     return (
         <div className="burger-stack">
-            <img src={topBunPng} />
+            <img src={topBunSelect} style={ingredients.length === 0 ? { zIndex: 100 } : {}} />
             {ingredients.slice().reverse().map((ing, i) => (
                 <img key={ing.id} src={ing.icon} style={{ zIndex: ingredients.length - i }} />
             ))}
